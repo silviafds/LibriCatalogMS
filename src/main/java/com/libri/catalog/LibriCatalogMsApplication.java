e6153fbb4a0aeed803d2b912a07f4ba4b420b73e
silvi <silviagfernandes1@gmail.com>
2025-12-28 13:34:24 -0300

    feat: first commit

---
+++ src/main/java/com/libri/catalog/LibriCatalogMsApplication.java
@@ -0,0 +1,13 @@
+package com.libri.catalog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibriCatalogMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibriCatalogMsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/libri/catalog/LibriCatalogMsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.libri.catalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibriCatalogMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
