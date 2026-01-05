5d19e866b1ebb0527e13b493fc2204ac36c1f7ca
silvi <silviagfernandes1@gmail.com>
2026-01-05 13:44:21 -0300

    fix: application file fix

--- src/main/java/com/libri/catalog/adapters/in/web/dto/response/BookRegistrationResponse.java
+++ src/main/java/com/libri/catalog/adapters/in/web/dto/response/BookRegistrationResponse.java
@@ -1,9 +1,6 @@
 package com.libri.catalog.adapters.in.web.dto.response;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 
 @Data
 public class BookRegistrationResponse {
